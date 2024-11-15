3cdb7fe8c5b7babae88b44c6a26c196640b7a2bc
Code Help Tutor <1823890830@qq.com>
2024-11-15 04:35:00 +0000

    WeChat cstutorcs COMP1027 CW1

---
+++ Part 3/3.1_Assembly/Power.asm
@@ -0,0 +1,21 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+
+// ================================= POWER ==================================
+// This program calculates the Exponential Power of a given number n 
+// At run time:
+//	- The user should enter the value of base (n), i.e., RAM[0]
+//	- The user should enter the value of power (e), i.e., RAM[1]
+//	- The program then calculate the e power of n, i.e., P(n,e).
+//		 For Example. If n = 2 and e = 5 than P(n,e) would be = 2*2*2*2*2 = 32
+//	- The result P(n,e) should be saved in RAM[2]
+// SPECIAL CASE:
+// If exponent power is ZERO, 
+// your program should store the ONE as answer in RAM[2] without doing any processing and end the program.
+// and ends the program. 
+// ==============================================================================
+
+// put your code here
+
+
